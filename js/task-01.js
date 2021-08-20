cdb5a09faf6e251a3211917f7996bcce8efeccd5
Maxim Ostrovsky <maxim.ostrovskiy86@gmail.com>
2021-08-20 13:16:22 +0300

    add task 8

--- js/task-01.js
+++ js/task-01.js
@@ -9,3 +9,53 @@ allCatEl.forEach(item => {
     console.log(`Категория: ${title}, Количество элементов: ${allListRef}`);
 })
 
+
+const user = {
+    name: 'Mango',
+    showName(event) {
+        console.log(this);
+        console.log(event.target)
+        // this будет ссылаться на button если использовать showName как callback
+        console.log(`My name is: ${this.name}`);
+        // тут undefined так как поля name у button нет
+    },
+};
+
+/*
+ * Представим что у нас есть кнопка с классом js-btn
+ * Выберем ее и повесим на нее слушатель клика
+ */
+const btn = document.querySelector('.js-btn');
+
+// user.showName(); //работает
+btn.addEventListener('click', user.showName); // не работает
+// btn.addEventListener('click', user.showName.bind(user)); // работае
+
+
+const form = document.querySelector(".form");
+const loginInput = form.querySelector('input[type="text"]');
+const passInput = form.querySelector('input[type="password"]');
+
+form.addEventListener("click", handleSubmit);
+
+function handleSubmit(event) {
+    // Предотвращаем поведение по умолчанию
+    event.preventDefault();
+    // console.log(event.target)
+    // console.log(event.currentTarget)
+
+    const login = loginInput.value.trim();
+    const password = passInput.value.trim();
+
+    if(login === '' || password === '') {
+        return alert('Пожалуйста введите валидную информацию!')
+    }
+
+    form.reset();
+
+    alert(`
+    Спасибо за регистрацию!
+    Логин: ${login} 
+    Пароль: ${password}
+  `);
+}
--- js/task-08.js
+++ js/task-08.js
@@ -1,31 +1,66 @@
-// ЮРА, Ещё не закончил в процесе
-
 const refs = {
-    // inputValue: document.querySelector('input').value,
+    inputValue: document.querySelector('input'),
     addCollection: document.querySelector('button[data-action="render"]'),
     removeCollection: document.querySelector('button[data-action="destroy"]'),
     output: document.querySelector('#boxes'),
 }
 
-const amount = document.querySelector('input').value;
+const colors = [
+    {rgb: '244, 67, 54'},
+    {rgb: '233, 30, 99'},
+    {rgb: '156, 39, 176'},
+    {rgb: '103, 58, 183'},
+    {rgb: '63, 81, 181'},
+    {rgb: '33, 150, 243'},
+    {rgb: '0, 188, 212'},
+    {rgb: '0, 150, 136'},
+    {rgb: '76, 175, 80'},
+    {rgb: '255, 235, 59'},
+    {rgb: '255, 152, 0'},
+];
+
+const handleDivAdd = () => {
+    const amountBox = +refs.inputValue.value;
+    const createDiv = createBoxes(amountBox);
+
+    refs.output.append(...createDiv);
+}
 
 const createBoxes = (amount) => {
-    // amount= +refs.inputValue.value;
-    console.log(amount)
-    const div = document.createElement('div');
-    div.style.width = 30 + 'px';
-    div.style.height = 30 + 'px';
-    div.style.backgroundColor = 'tomato';
+    let divSize = 30;
+    const arrayDiv = [];
 
     for (let i = 0; i < amount; i++) {
-        refs.output.append(div)
+        const div = document.createElement('div');
+        divSize += 10;
+        div.style.cssText = `
+        width: ${divSize + 'px'};
+        height: ${divSize + 'px'};
+        margin: 10px;
+        `;
+        div.style.backgroundColor = randomColor();
+        div.style.margin = '5px';
+
+        console.log(div.style.cssText)
+        arrayDiv.push(div);
     }
+
+    return arrayDiv;
+}
+
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+const randomColor = () => {
+    const arrayColors = colors.map(color => color.rgb);
+    return `rgb(${arrayColors[randomInteger(0, arrayColors.length - 1)]})`;
 }
 
 const destroyBoxes = () => {
-   refs.output.innerHTML = '';
+    refs.output.innerHTML = '';
 }
 
-console.log(+refs.inputValue)
-refs.addCollection.addEventListener('click', createBoxes);
+refs.addCollection.addEventListener('click', handleDivAdd);
 refs.removeCollection.addEventListener('click', destroyBoxes);
